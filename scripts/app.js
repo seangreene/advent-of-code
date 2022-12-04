695e2b10791c344a47f278549481b2db1b95e609
Sean Greene <seangreene89@gmail.com>
2022-12-04 13:58:43 -0800

    Day 3 Part 1

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ http
   })
   .listen(8080);
 
-const file = '../_data/day-two-data.txt';
+const file = '../_data/day-three-data.txt';
 
 fs.readFile(file, function (err, data) {
   if (err) throw err;
@@ -17,122 +17,51 @@ fs.readFile(file, function (err, data) {
   //each grouping of values represents one elf's inventory
   const raw = data.toString().split('\n');
 
-  // DAY TWO: Rock Paper Scissors
-  // What would your total score be if everything goes exactly according to your strategy guide?
+  // DAY THREE: Rucksack Reorganization
 
-  // parse the strategy guide data
+  // A given rucksack always has the same number of items in each of its two compartments, so the first half of the characters represent items in the first compartment, while the second half of the characters represent items in the second compartment.
+  const rucksacks = raw.map((d) => {
+    let l = d.length;
+    let m = l / 2;
 
-  // function to parse letters into rock, paper or scissors
+    c1 = d.slice(0, m).split('', l);
+    c2 = d.slice(m, l).split('', l);
 
-  // A = Rock, B = Paper, C = Scissors
-  // X = Lose, Y = Draw, Z = Win
-
-  function parseChoice(l) {
-    if (l == 'A') {
-      return 'rock';
-    } else if (l == 'B') {
-      return 'paper';
-    } else if (l == 'C') {
-      return 'scissors';
-    } else {
-      return 'farts';
-    }
-  }
-
-  function parseResult(l) {
-    if (l == 'X') {
-      return 'lose';
-    } else if (l == 'Y') {
-      return 'draw';
-    } else if (l == 'Z') {
-      return 'win';
-    } else {
-      return 'farts';
-    }
-  }
-
-  // parse input data
-  const strategyGuide = raw.map((d) => {
-    let round = d.split(' ');
-    let obj = {
-      elf_choice: parseChoice(round[0]),
-      result_needed: parseResult(round[1]),
-    };
-    return obj;
+    return [c1, c2];
   });
 
-  // choose rock, paper or scissors based on the required result
-  function makeChoice(r) {
-    let e = r['elf_choice'];
-    let o = r['result_needed'];
-
-    let my_choice;
-
-    if (o == 'win') {
-      if (e == 'rock') {
-        my_choice = 'paper';
-      } else if (e == 'paper') {
-        my_choice = 'scissors';
-      } else if (e == 'scissors') {
-        my_choice = 'rock';
-      }
-    } else if (o == 'lose') {
-      if (e == 'rock') {
-        my_choice = 'scissors';
-      } else if (e == 'paper') {
-        my_choice = 'rock';
-      } else if (e == 'scissors') {
-        my_choice = 'paper';
-      }
-    } else {
-      my_choice = e;
-    }
-    return my_choice;
-  }
-
-  // let the game begin
-  strategyGuide.map((d) => {
-    d.my_choice = makeChoice(d);
+  // Find common item in each compartment
+  //const filteredArray = rucksacks[0][0].filter(value => rucksacks[0][1].includes(value));
+  let commonItems = [];
+  rucksacks.forEach((d, i) => {
+    commonItems.push(d[0].filter((v) => d[1].includes(v)));
   });
 
-  //Your total score is the sum of your scores for each round. The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+  // To help prioritize item rearrangement, every item type can be converted to a priority:
 
-  // Points:
-  // Rock = 1, Paper = 2, Scissors = 3
-  // Loss = 0, Draw = 3, Win = 6
-  const wdlPoints = {
-    win: 6,
-    draw: 3,
-    lose: 0,
-  };
-  const bonusPoints = {
-    rock: 1,
-    paper: 2,
-    scissors: 3,
-  };
+  // Lowercase item types a through z have priorities 1 through 26.
+  // Uppercase item types A through Z have priorities 27 through 52.
 
-  // count points
-  let myScore = 0;
-  let elfScore = 0;
+  // test if char is upper case
+  const isUpperCase = (string) => /^[A-Z]*$/.test(string);
 
-  strategyGuide.forEach((d, i) => {
-    myScore += wdlPoints[d.result_needed] + bonusPoints[d.my_choice];
+  // condition ? if true do this : if false do this
 
-    if (d.result_needed == 'lose') {
-      elfScore += 6 + bonusPoints[d.elf_choice];
-    } else if (d.result_needed == 'draw') {
-      elfScore += 0 + bonusPoints[d.elf_choice];
-    } else {
-      elfScore += bonusPoints[d.elf_choice];
-    }
-  });
-
-  function crownWinner(a, b) {
-    if (a > b) {
-      return `I win, ${a} to ${b}! Yay!`;
-    } else {
-      return `Elf wins, ${a}-${b}. Boo!`;
-    }
+  function priortizer(str) {
+    return isUpperCase(str)
+      ? str.toString().charCodeAt(0) - 38
+      : str.toString().charCodeAt(0) - 96;
   }
-  console.log(crownWinner(myScore, elfScore));
+
+  // What is the sum of the priorities of those item types?
+
+  let prioritizedItems = commonItems
+    .map((d) => {
+      return priortizer(d[0]);
+    })
+    .reduce((i, d) => {
+      return i + d;
+    });
+
+  console.log(prioritizedItems);
 });
